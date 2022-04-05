06c3c900cd1eed57dd9b86bf00489c5f4d61f4aa
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-06 01:32:31 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3000
 
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
