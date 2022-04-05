4bed96a4ab29ef82bb674ad4b28d2ea3977682f9
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-06 00:16:05 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,8 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Aj!')
-})
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
