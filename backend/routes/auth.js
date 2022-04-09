4fdb951ccb6f7bb1877b82731b481b6e32d6bebd
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-10 02:39:07 +0530

    updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     const user = User(req.body);
     user.save();
     res.send("hello")
