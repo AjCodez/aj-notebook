b1e73d4299945d3eed129d3b45cfc66b3fb137fe
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-05-02 23:33:58 +0530

    updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,14 +3,14 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator')
 
-router.post('/',  [
-    body('email').isEmail(),
+router.post('/', [
+  body('email').isEmail(),
 ], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    res.send(res.body)
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send(res.body)
 })
 
 module.exports = router;
