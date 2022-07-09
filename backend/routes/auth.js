316ffce4924276530bd44861c96fb2eac5db8e3f
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-09 23:11:05 +0530

    validations

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator')
 
 router.get('/',[
-  body('email'),isEmail(),
-  body('name').isLength({ min: 3 }),
-  body('password').isLength({ min: 8 }),
+  body('email','enter a valid email').isEmail(),
+  body('name', 'length should be greater than 3').isLength({ min: 3 }),
+  body('password', 'length should be greater than 8').isLength({ min: 8 }),
 ], (req, res) => {
   console.log(req.body);
   const errors = validationResult(req);
