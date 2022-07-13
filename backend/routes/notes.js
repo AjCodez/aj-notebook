2febc789e761f922743ffd8037ed520f11518f20
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-14 03:54:11 +0530

    notes schema

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router();
+const fetchUser = require('../middleware/fetchUser');
+const Notes = require('../models/Notes');
 
-router.get('/', (req, res) => {
-    res.json([])
+router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    notes = await Notes.find({user: req.user.id});
+    res.json(notes)
 })
 
 module.exports = router;
