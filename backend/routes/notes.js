7616cef950a93ddfb5940bc214c9bcccb75d3e53
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-14 04:06:58 +0530

    add note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,10 +2,37 @@ const express = require('express')
 const router = express.Router();
 const fetchUser = require('../middleware/fetchUser');
 const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
 router.get('/fetchallnotes', fetchUser, async (req, res) => {
-    notes = await Notes.find({user: req.user.id});
-    res.json(notes)
+    try {
+        notes = await Notes.find({ user: req.user.id });
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error happend")
+    }
+})
+
+router.get('/addnote', fetchUser, [
+    body('title', 'enter a valid title').exists(),
+    body('description', 'enter something to note description').exists(),
+], async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { title, description, tag } = req.body;
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+        res.json(savedNote)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error happend")
+    }
 })
 
 module.exports = router;
