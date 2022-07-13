3503e87ff075357c25925d6ae8b6f104ca8c62c5
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-14 00:22:34 +0530

    login user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 router.post('/createuser', [
   body('email', 'enter a valid email').isEmail(),
@@ -26,10 +27,36 @@ router.post('/createuser', [
       email: req.body.email,
       password: securePass,
     })
-    res.json(user)
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, 'shhhhh');
+
+    res.json({authToken})
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Some error happend")
+  }
+})
+
+router.post('/loginuser', [
+  body('email', 'enter a valid email').isEmail(),
+  body('name', 'length should be greater than 3').exists(),
+], async (req, res) => {
+  console.log(req.body);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    let user = await User.findOne({email});
+    if(!user){
+      return res.send(400).json({error: "Invalid credentials"});
+    }
+  } catch (error) {
   }
 })
 
