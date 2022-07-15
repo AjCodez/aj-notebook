330dad525e79817335d0fc47fa1680865793a7f4
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-16 01:42:40 +0530

    useContext

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/NoteContext'
 
 export const About = () => {
-  return (
-    <div>This is About</div>
-  )
+    const context = useContext(noteContext);
+    return (
+        <div>This is About {context.name} who is in {context.year} year of b.tech </div>
+    )
 }
