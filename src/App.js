ae153c560f64a72e64b8f4613c9c59a809e79bb7
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-16 00:45:04 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React from "react";
 import { Navbar } from './components/navbar';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Home } from './components/home';
+import { About } from './components/about';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-      <h4>Aj-Notebook</h4>
-    </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
