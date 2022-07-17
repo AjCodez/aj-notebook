2ca4208e3051a89087669c48e0d424d940c61f81
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-18 04:35:03 +0530

    updated

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -59,7 +59,15 @@ const NoteState = (props) => {
     ]
 
     const addNote = (title, description, tag) => {
-        const notes = null;
+        const notes = {
+            "_id": "62d42a257926486f149e39a47",
+            "user": "62cdf02b25fea4c69957eda3",
+            "title": "My second note",
+            "description": "Second note created with api",
+            "tag": "personal",
+            "date": "2022-07-17T15:27:19.249Z",
+            "__v": 0
+        };
         setNote(initialNotes.push(notes))
     }
 
