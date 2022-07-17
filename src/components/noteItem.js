3e42e2864e4d75aa5e34d4695b084886dab0cd74
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-18 02:55:32 +0530

    Notes

--- src/components/noteItem.js
+++ src/components/noteItem.js
@@ -4,11 +4,9 @@ export default function NoteItem(props) {
     return (
         <div>
             <div className="card container my-3">
-                <img src="" className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{props.note.title}</h5>
                         <p className="card-text">{props.note.description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
             </div>
         </div>
