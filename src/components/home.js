6b951bc15d51d8813255bcd840a19ecadf9c407d
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-23 00:29:57 +0530

    updated

--- src/components/home.js
+++ src/components/home.js
@@ -32,10 +32,6 @@ export const Home = () => {
             <label htmlFor="description" className="form-label">Description</label>
             <input type="text" className="form-control" id="description" name="description" onChange={onChange} />
           </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-          </div>
           <button type="submit" className="btn btn-primary" onClick={handleClick}>Add the note</button>
         </form>
       </div>
