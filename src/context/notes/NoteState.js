5611a79db4e89e4bae95fc69e259a8fa6e5529a4
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-22 18:24:14 +0530

    fetch

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,64 +1,23 @@
 import { useState } from "react";
 import NoteContext from "./NoteContext";
 
-const NoteState = (props) => {
+const NoteState = async (props) => {
 
-    const initialNotes = [
-        {
-            "_id": "62c2f4aa68d45d87d7d67630a",
-            "user": "62cdf02b25fea4c69957eda3",
-            "title": "My first note",
-            "description": "First note created with api",
-            "tag": "personal",
-            "date": "2022-07-13T22:43:50.634Z",
-            "__v": 0
-        },
-        {
-            "_id": "62d42a57926486f149e39a247",
-            "user": "62cdf02b25fea4c69957eda3",
-            "title": "My second note",
-            "description": "Second note created with api",
-            "tag": "personal",
-            "date": "2022-07-17T15:27:19.249Z",
-            "__v": 0
-        }, {
-            "_id": "62cf4aa68d45d87d27d67630a",
-            "user": "62cdf02b25fea4c69957eda3",
-            "title": "My first note",
-            "description": "First note created with api",
-            "tag": "personal",
-            "date": "2022-07-13T22:43:50.634Z",
-            "__v": 0
-        },
-        {
-            "_id": "62d242a57926486f149e39a47",
-            "user": "62cdf02b25fea4c69957eda3",
-            "title": "My second note",
-            "description": "Second note created with api",
-            "tag": "personal",
-            "date": "2022-07-17T15:27:19.249Z",
-            "__v": 0
-        }, {
-            "_id": "62cf4aa68d45d287d7d67630a",
-            "user": "62cdf02b25fea4c69957eda3",
-            "title": "My first note",
-            "description": "First note created with api",
-            "tag": "personal",
-            "date": "2022-07-13T22:43:50.634Z",
-            "__v": 0
-        },
-        {
-            "_id": "62d42a257926486f149e39a47",
-            "user": "62cdf02b25fea4c69957eda3",
-            "title": "My second note",
-            "description": "Second note created with api",
-            "tag": "personal",
-            "date": "2022-07-17T15:27:19.249Z",
-            "__v": 0
-        }
-    ]
+    const getNote = () => {
+        const response = await fetch("localhost:5000/api/notes/fetchallnotes", {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJjZGYwMmIyNWZlYTRjNjk5NTdlZGEzIn0sImlhdCI6MTY1NzczOTExN30.PD_iHlUk6u3LiWXNA-LMObjjwtWWHp69zrvPVQoAx4E'
+            }, body: JSON.stringify(data)
+        });
+        const initialNotes = response.json();
+    }
 
     const [note, setNote] = useState(initialNotes)
+
+
 
     const addNote = (title, description, tag) => {
         const notes = {
@@ -78,15 +37,15 @@ const NoteState = (props) => {
         setNote(filteredNote);
     }
 
-    const updateNote = (id,title, description,tag) => { 
+    const updateNote = (id, title, description, tag) => {
         for (let index = 0; index < note.length; index++) {
             const element = note[index];
-            if(element._id===id){
-                element.title=title;
-                element.description=description;
-                element.tag=tag;
+            if (element._id === id) {
+                element.title = title;
+                element.description = description;
+                element.tag = tag;
             }
-            
+
         }
     }
 
@@ -95,7 +54,7 @@ const NoteState = (props) => {
 
 
     return (
-        <NoteContext.Provider value={{ note, addNote, deleteNote, updateNote }}>
+        <NoteContext.Provider value={{ note, addNote, deleteNote, updateNote, getNote }}>
             {props.children}
         </NoteContext.Provider>
     )
