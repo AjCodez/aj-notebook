0daef3eb67fea67cf2e924df50ff2430c0eabfb3
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-23 20:39:47 +0530

    tag option

--- src/components/home.js
+++ src/components/home.js
@@ -15,7 +15,7 @@ export const Home = () => {
   }
   const handleClick = (e) => {
     e.preventDefault();
-    addNote(note.title, note.description, 'personal')
+    addNote(note.title, note.description, note.tag)
   }
 
   return (
@@ -31,6 +31,10 @@ export const Home = () => {
           <div className="mb-3">
             <label htmlFor="description" className="form-label">Description</label>
             <input type="text" className="form-control" id="description" name="description" onChange={onChange} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="tag" className="form-label">Tag</label>
+            <input type="text" className="form-control" id="tag" name="tag" onChange={onChange} />
           </div>
           <button type="submit" className="btn btn-primary" onClick={handleClick}>Add the note</button>
         </form>
