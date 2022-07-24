6bf32fb8309986ad2128601657941ad555c240aa
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-25 03:32:22 +0530

    setting up update note

--- src/components/noteItem.js
+++ src/components/noteItem.js
@@ -3,7 +3,7 @@ import noteContext from '../context/notes/NoteContext'
 
 export default function NoteItem(props) {
     const context = useContext(noteContext)
-    const { deleteNote } = context
+    const { deleteNote, updateNote } = context
     return (
         <div className='col-md-4'>
             <div className="card my-3">
@@ -12,7 +12,7 @@ export default function NoteItem(props) {
 
                         <h5 className="card-title">{props.note.title}</h5>
                         <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(props.note._id) }}></i>
-                        <i className="fa-solid fa-pen-to-square mx-2"></i>
+                        <i className="fa-solid fa-pen-to-square mx-2" onClick={() => { }}></i>
                     </div>
                     <p className="card-text">{props.note.description}</p>
                 </div>
