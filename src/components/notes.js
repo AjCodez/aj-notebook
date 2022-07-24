41550da14b51cb315b2cd95d298934865ecfe28b
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-25 03:47:25 +0530

    updated

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,20 +1,26 @@
 import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/NoteContext'
 import NoteItem from './noteItem'
+import { UpdateNoteModal } from './updateNoteModal'
 
 export default function Notes() {
     const context = useContext(noteContext)
     const { note, getNote } = context
     useEffect(() => {
       getNote();
-    }, [])
+    }, [getNote])
+
+    const updateNote = (note) => {
+
+    }
     
     return (
         <div className='row my-3'>
+            <UpdateNoteModal/>
 
             <h1>Your notes</h1>
             {note.map((note) => {
-                return <NoteItem key={note._id} note={note} />
+                return <NoteItem key={note._id} updateNote={updateNote} note={note} />
             })}
         </div>
     )
--- src/components/updateNoteModal.js
+++ src/components/updateNoteModal.js
@@ -3,12 +3,12 @@ import React from 'react'
 export const UpdateNoteModal = () => {
     return (
         <div>
-            // Button trigger modal
+            
             <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
             </button>
 
-            // Modal
+            
             <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
